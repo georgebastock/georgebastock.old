c751ba86ce7b7bfe5b1faf5664f3173f1bde8f46
George Bastock <42344811+georgebastock@users.noreply.github.com>
2019-05-14 16:40:02 +0100

    Content push

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+// -----------------------------------------------------------------------------------------------
+// This file contains:
+//                      JS file with all custom built functionality to make the site reactive
+//                      to the user.
+// -----------------------------------------------------------------------------------------------
+
+
+// Time Title Changer - On Page Load
+    let heroTitleElement = document.querySelector('#adaptiveTitle');
+
+    let currentHour = new Date().getHours();
+
+    let possibleTextDisplay = ["Good Morning!", "Good Afternoon!", "Good Evening!", "Good Night!", "Check Console!"]
+
+    if (currentHour != null) {
+        if (currentHour >= 4 && currentHour < 12) {
+            heroTitleElement.textContent = possibleTextDisplay[0];
+        } else if (currentHour >= 12 && currentHour < 17) {
+            heroTitleElement.textContent = possibleTextDisplay[1];
+        } else if (currentHour >= 17 && currentHour < 21) {
+            heroTitleElement.textContent = possibleTextDisplay[2];
+        } else {
+            heroTitleElement.textContent = possibleTextDisplay[3];
+        }
+    } else {
+        heroTitleElement.textContent = possibleTextDisplay[4];
+    }
+
+// Typing Sub Title
+    var options = {
+        strings: ["Front End Developer.", "Web Designer.", "Cat Enthusiast.", "Web Developer.", "PC Builder.", "Web Warrior.", "Good Looking Dude."],
+        typeSpeed: 80,
+        backSpeed: 50,
+        shuffle: true,
+        backDelay: 2000,
+        startDelay: 200,
+        loop: true
+    }
+
+    var typed = new Typed("#typed", options);
+
+// Lettering Effect
+    $("h1").html(
+        $("h1").text().split("").map(a => `<span class="letter">${a}</span>`)
+    )
+
+// Disable right click
+//$(document).on("contextmenu", function (event) { event.preventDefault(); });
+
+// Logs to console when loaded into the site
 console.log("LOADED");
