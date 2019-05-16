7bb87c86801aa565e6d49de626ff2c5b001405b7
George Bastock <42344811+georgebastock@users.noreply.github.com>
2019-05-16 16:34:25 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -26,13 +26,19 @@
 
 // Typing Sub Title
     var options = {
-        strings: ["Front End Developer.", "Web Designer.", "Cat Enthusiast.", "Web Developer.", "PC Builder.", "Web Warrior.", "Good Looking Dude."],
+        strings: [
+            "I <i class='fas fa-heart'></i> Being A Front End Developer.", 
+            "I <i class='fas fa-heart'></i> Being A Web Designer.", 
+            "I <i class='fas fa-heart'></i> Being A PC Builder.", 
+            "I <i class='fas fa-heart'></i> Being A Web Warrior."], 
         typeSpeed: 80,
         backSpeed: 50,
         shuffle: true,
         backDelay: 2000,
         startDelay: 1600,
-        loop: true
+        loop: true,
+        contentType: 'html',
+        smartBackspace: true
     }
 
     $(document).ready(function(){
@@ -54,13 +60,23 @@
         $(this).removeClass("hoverAnimation");
     })
 
-// Heart animation on click
-    $('.fa-heart').click(function(){
-        $(this).toggleClass('vibe');
+// Navbar scroll shrink / Scroll hide
+    $(document).ready(function($) {
+        $(window).on('scroll touchmove', function () {
+            $('#navbar').toggleClass('small', ($(document).scrollTop() > 400));
+            $('.scroll-indicator').toggleClass('hidden', ($(document).scrollTop() > 400))
+        }).scroll();
+    });
+
+// Fullpage Scroll
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            licenseKey: 'YOUR_KEY_HERE',
+            anchors: ['homepage', 'projects'],
+            autoScrolling: true,
+            scrollBar: true
+        });
     });
 
 // Disable right click
 //$(document).on("contextmenu", function (event) { event.preventDefault(); });
-
-// Logs to console when loaded into the site
-console.log("LOADED");
