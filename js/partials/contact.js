0554927c8566569a45b32b4f8d1c1e68ff4918f8
George Bastock <gbastock@outlook.com>
2020-03-07 23:09:10 +0000

    update commit

--- js/partials/contact.js
+++ js/partials/contact.js
@@ -1,25 +1,4 @@
 $(document).ready(function(){
-    // Google Maps
-        var selector_map = $('#map');
-        var draggable = selector_map.attr('data-draggable');
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 52.6309, lng: 1.2974},
-            zoom: 14,
-            disableDefaultUI: true,
-            scrollwheel: false,
-            zoomControl: false,  
-            disableDoubleClickZoom: true,
-            navigationControl: true,
-            mapTypeControl: false,
-            scaleControl: false,
-            draggable: draggable
-        });
-
-        map.setOptions({
-            draggableCursor:'default'
-        });
-
     // Form
         $('.contact-form').on('submit', function(e) {
             $(e.target[3]).val("Sending...");
